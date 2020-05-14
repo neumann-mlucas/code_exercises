4d1e19bdf184b45036a12efbadc8d98c40f60b81
Lucas Neumann <neumannlucas@gmail.com>
2020-05-14 01:06:46 -0300

    first commit

---
+++ rosalind/src/rosalind_prot.py
@@ -0,0 +1,98 @@
+from itertools import tee
+
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def translate(seq):
+    codons = (seq[i:j] for i, j in pairwise(range(0, len(seq) + 1, 3)))
+    aa_seq = "".join(codon_table[c] for c in codons)
+    # Split sequence at a stop codon ('!')
+    return aa_seq[: aa_seq.find("!")]
+
+
+DATA_FILE = "dat/rosalind_prot.txt"
+
+SAMPLE_DATA = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
+SAMPLE_OUTPUT = "MAMAPRTEINSTRING"
+
+codon_table = {
+    "UUU": "F",
+    "CUU": "L",
+    "AUU": "I",
+    "GUU": "V",
+    "UUC": "F",
+    "CUC": "L",
+    "AUC": "I",
+    "GUC": "V",
+    "UUA": "L",
+    "CUA": "L",
+    "AUA": "I",
+    "GUA": "V",
+    "UUG": "L",
+    "CUG": "L",
+    "AUG": "M",
+    "GUG": "V",
+    "UCU": "S",
+    "CCU": "P",
+    "ACU": "T",
+    "GCU": "A",
+    "UCC": "S",
+    "CCC": "P",
+    "ACC": "T",
+    "GCC": "A",
+    "UCA": "S",
+    "CCA": "P",
+    "ACA": "T",
+    "GCA": "A",
+    "UCG": "S",
+    "CCG": "P",
+    "ACG": "T",
+    "GCG": "A",
+    "UAU": "Y",
+    "CAU": "H",
+    "AAU": "N",
+    "GAU": "D",
+    "UAC": "Y",
+    "CAC": "H",
+    "AAC": "N",
+    "GAC": "D",
+    "UAA": "!",
+    "CAA": "Q",
+    "AAA": "K",
+    "GAA": "E",
+    "UAG": "!",
+    "CAG": "Q",
+    "AAG": "K",
+    "GAG": "E",
+    "UGU": "C",
+    "CGU": "R",
+    "AGU": "S",
+    "GGU": "G",
+    "UGC": "C",
+    "CGC": "R",
+    "AGC": "S",
+    "GGC": "G",
+    "UGA": "!",
+    "CGA": "R",
+    "AGA": "R",
+    "GGA": "G",
+    "UGG": "W",
+    "CGG": "R",
+    "AGG": "R",
+    "GGG": "G",
+}
+
+# Read data
+with open(DATA_FILE, "r") as f:
+    seq = f.readline().strip()
+
+# Assert sample
+assert translate(SAMPLE_DATA) == SAMPLE_OUTPUT
+
+# Produce output
+print(translate(seq))
