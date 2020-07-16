a25246db77dd2ae7cbc5fc87e725ffa3b73e8786
Lucas Neumann <neumannlucas@gmail.com>
2020-07-16 16:33:24 -0300

    first commit

---
+++ project_euler/src/euler_025.jl
@@ -0,0 +1,9 @@
+helper(current :: BigInt, next :: BigInt, n :: BigInt) = n == 0 ? current : helper(next, next + current, n - 1)
+tfib(n) = helper(BigInt(0), BigInt(1), BigInt(n))
+
+function answer(range)
+    ns = map(tfib, range)
+    filter(x->length(string(x)) < 1000, ns) |> x->length(x) +1
+end
+
+answer(1:5000) |> print
