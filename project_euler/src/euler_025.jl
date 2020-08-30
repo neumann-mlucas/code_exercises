d4e805f7dfdbe6db2ae909e4d5bb5de61721872a
Lucas Neumann <neumannlucas@gmail.com>
2020-08-30 00:40:00 -0300

    lazy takewhile

--- project_euler/src/euler_025.jl
+++ project_euler/src/euler_025.jl
@@ -1,10 +1,12 @@
+import Base.Iterators: countfrom, takewhile
+
 helper(current::BigInt, next::BigInt, n::BigInt) =
     n == 0 ? current : helper(next, next + current, n - 1)
 tfib(n) = helper(BigInt(0), BigInt(1), BigInt(n))
 
-function answer(range)
-    ns = map(tfib, range)
-    filter(x -> length(string(x)) < 1000, ns) |> x -> length(x) + 1
+function answer(lim)
+    predicate(n) = length(string(tfib(n))) < lim
+    takewhile(predicate, countfrom()) |> collect |> x -> last(x) + 1
 end
 
-answer(1:5000) |> print
+answer(1000) |> print
