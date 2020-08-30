d428ef22aef53c94be80e62de47de3b4f3fce5ea
Lucas Neumann <neumannlucas@gmail.com>
2020-08-30 00:08:56 -0300

    change to function name

--- project_euler/src/euler_003.jl
+++ project_euler/src/euler_003.jl
@@ -1,7 +1,7 @@
-isprime(n) = map(x -> n % x != 0, 2:isqrt(n)) |> all
+is_prime(n) = map(x -> n % x != 0, 2:isqrt(n)) |> all
 
 function answer(n)
-    filter(x -> (n % x == 0 && isprime(x)), 3:2:isqrt(n)) |> last
+    filter(x -> (n % x == 0 && is_prime(x)), 3:2:isqrt(n)) |> last
 end
 
 answer(600851475143) |> print
