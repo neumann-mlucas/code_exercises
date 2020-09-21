2e498e04bc334661ae42665aaf101d2bc76f645b
Lucas Neumann <neumannlucas@gmail.com>
2020-09-21 14:31:01 -0300

    first commit

---
+++ project_euler/src/euler_052.jl
@@ -0,0 +1,12 @@
+import Base.Iterators: first, filter
+
+multiples(n) = map(x -> digits(x * n), 1:6)
+same_digits(m) = all(x -> x == sort(m[1]), sort.(m))
+
+function answer(lim)
+    predicate(n) = same_digits(multiples(n))
+    filter(predicate, 1:lim) |> first
+end
+
+
+answer(1_000_000) |> print
