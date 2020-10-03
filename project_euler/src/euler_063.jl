c5fb141ac8746aee07c1fcb6a8d6615414f31e28
Lucas Neumann <neumannlucas@gmail.com>
2020-10-02 23:26:14 -0300

    first commit

---
+++ project_euler/src/euler_063.jl
@@ -0,0 +1,14 @@
+import Base.Iterators: dropwhile, filter, takewhile
+
+function fn(power)
+    (BigInt(n)^power for n = 1:100_000) |>
+    i ->
+        dropwhile(x -> ndigits(x) < power, i) |>
+        i -> takewhile(x -> ndigits(x) == power, i) |> i -> length(collect(i))
+end
+
+function answer(lim)
+    map(fn, 1:lim) |> sum
+end
+
+answer(25) |> print
