8945dd044af5876bff29ca4da980d766a50b43f1
Lucas Neumann <neumannlucas@gmail.com>
2020-10-24 16:48:23 -0300

    first commit

--- project_euler/src/euler_093.jl
+++ project_euler/src/euler_093.jl
@@ -4,15 +4,19 @@ using Base.Iterators
 function apply(numbers, functions)
     f1, f2, f3 = functions
     a, b, c, d = numbers
-    f1(a, b) |> x -> f2(x, c) |> x -> f3(x, d)
+
+    o1 = f1(a, b) |> x -> f2(x, c) |> x -> f3(x, d)
+    o2 = f1(a, b) |> x -> f2(x, c) |> x -> f3(d, x)
+    o3 = f1(a, b) |> x -> f2(c, x) |> x -> f3(x, d)
+    o4 = f1(a, b) |> x -> f2(c, x) |> x -> f3(d, x)
+    max(o1, o2, o3, o4)
 end
 
 function unique_solutions(numbers)
     ops = [+, -, *, /]
     p_ops = product(ops, ops, ops) |> collect
     function helper(perm)
-        (apply(perm, f) for f in p_ops) |>
-        l -> (Int(abs(n)) for n in l if isinteger(n) && n != 0)
+        (apply(perm, f) for f in p_ops) |> l -> (Int(n) for n in l if isinteger(n) && n > 0)
     end
     (helper(p) for p in permutations(numbers)) |> flatten |> unique |> sort
 end
